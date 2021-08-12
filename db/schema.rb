3779c68b40b63f6f5c8942bdf80007dc4acf7259
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-12 10:54:55 -0300

    list articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_141117) do
+ActiveRecord::Schema.define(version: 2021_08_12_131720) do
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
