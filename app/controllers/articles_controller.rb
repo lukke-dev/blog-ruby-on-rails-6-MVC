bd3363f809c54d4fda6f473ed0e19ac6a0bcf969
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-14 00:48:20 -0300

    add create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title,:body)
+  end
 end
